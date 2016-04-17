0ded67854af6e0a16ba052f614455b2f67a750bf
Owen Craig <owen@owencraig.com>
2016-04-17 12:14:02 -0500

    Serialised http requests, normalised API to use COOL_DOWN and extedned readme

--- README.md
+++ README.md
@@ -13,3 +13,23 @@ After building run ```docker run -p 3000:3000 --name="translator" --label="trans
 ## API
 
 Requests take the form of ```{HOST}:{PORT}/{To Language}/{From Language}/{Translation term}```
+
+And will return a JSON object of the form
+
+``` json
+{
+    "source": "<APP_NAME + start time>",
+    "original": "<input term>",
+    "translated": "<translated term or input in the case of no translation found>"
+}
+```
+
+## Environment Variables
+Variable | Default | Description
+--- | --- | ---
+COOL_DOWN | 0 | The time the app will block between requests
+APP_PORT | 3000 | The port the application will listen on
+APP_NAME | trans | The start of the of source in the response 
+
+## Notes
+The app is intentionally serial in its connections to showcase how scaling affects response times in an application.
--- index.js
+++ index.js
@@ -5,15 +5,22 @@ var getTranslation = require('./translate').getTranslation;
 var moment = require('moment');
 
 const APP_PORT = process.env.APP_PORT || 3000;
-const RESPONSE_DELAY = process.env.RESPONSE_DELAY || 1;
+const COOL_DOWN = process.env.COOL_DOWN || 0;
 const BASE_RESPONSE = Object.freeze({
    source: `${process.env.APP_NAME || 'trans' }: ${getDateString()}`
 });
 
+var promise = Promise.resolve();
+
 app.get('/:toLang/:fromLang/:term', (req, res) => {
     log(`Request received ${req.params.term}: ${req.params.fromLang} => ${req.params.toLang}`);
     let translator = getTranslation.bind(null, req.params.toLang, req.params.fromLang);
-    setTimeout(() => res.json(buildResponse(BASE_RESPONSE, translator, req.params.term)), RESPONSE_DELAY);
+
+    // simluate some long blocking process
+    promise = promise
+        .then(() => log(`Request processing ${req.params.term}: ${req.params.fromLang} => ${req.params.toLang}`))
+        .then(() => res.json(buildResponse(BASE_RESPONSE, translator, req.params.term)))
+        .then(() => pause(COOL_DOWN));
 });
 
 function buildResponse(baseResponse, translator, term){
@@ -33,5 +40,11 @@ function log(message){
     console.log(`${getDateString()}: ${message}`);
 }
 
+
+function pause(time){
+    return new Promise(function(resolve){
+        setTimeout(() => resolve(), time);
+    });
+}
 
 app.listen(APP_PORT, () => log(`translator listening on port ${APP_PORT}`));
